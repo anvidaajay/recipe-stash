cf52aa3ee6a9c521ddffdd7ef6e663944301e124
anvidaajay <anvidaajay.official@gmail.com>
2026-08-15 00:13:00 +0530

    Prepare Django settings for deployment

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -21,15 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    "DJANGO_SECRET_KEY",
-    "django-insecure-c&br1jd@4&g%z1lmcx%9joi&0-rb%ag-h9)y-#0e$_)%85z017"
-)
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
